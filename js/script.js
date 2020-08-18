408935d740237b401e8228e2355647c9a92541f9
kr.viku <er.vikashs4@gmail.com>
2020-08-18 12:39:34 +0530

    update some file

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Software Engineer.", "Web Developer.", "Student At University Institute Of Technology, Burdwan."],
+		strings: ["4⭐Coder on Codechef.","Software Engineer.", "MERN Stack Developer.", "Student At University Institute Of Technology, Burdwan."],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
